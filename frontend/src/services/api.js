8bef5243e42dd7112ddaedfd4797d8c3e18426bd
Sumukesh <sumukeshmopuram1@gmail.com>
2025-07-21 14:39:45 +0530

    added location

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:3001' 
-    : 'https://portfolio-r-gzgf.onrender.com',
+  baseURL: 'https://portfolio-r-gzgf.onrender.com',
   timeout: 5000,
   withCredentials: true,
 });
