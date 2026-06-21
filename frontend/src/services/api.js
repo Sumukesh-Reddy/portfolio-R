0c9fffad7ae61de896cbcc6e77c98cb8b40dbca5
Sumukesh <sumukeshmopuram1@gmail.com>
2026-06-21 16:06:06 +0530

    added app.py's live server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001'
-    : process.env.backend_url || 'https://portfolio-r-gzgf.onrender.com';
+    : process.env.backend_url || 'https://portfolio-r-1.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE,
